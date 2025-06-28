53fa570212154dc636a6256e81cee3e7fb948f6e
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-06-28 08:55:08 -0700

    defined luandry_app to my dj_folder

--- dj_folder/settings.py
+++ dj_folder/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'laundry_app',  # Custom app for laundry management
 ]
 
 MIDDLEWARE = [
