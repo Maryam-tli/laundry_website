71c06a54f4be1001802460b24fec6ab9ddfab879
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-06-28 09:23:21 -0700

    Configured STATIC and MEDIA settings

--- dj_folder/settings.py
+++ dj_folder/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'  # Directory for static files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # Directory for media files
+STATICFILS_DIRS = [
+    BASE_DIR / 'statics',  # Additional directory for static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
