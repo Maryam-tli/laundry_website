086bdebffe921fa8774c88039ab7fa1774189d39
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-06-29 07:56:15 -0700

    Added URL patterns for home, about, services, and pickup scheduling views

---
+++ laundry_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from laundry_app.views import *
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('about/', about_view, name='about'),
+    path('services/', services_view, name='services'),
+    path('schedule-pickup/', schedule_view, name='schedule_pickup')
+]
