b936cdc2da9bcd0bc21d8587ae81e53de8bc4d77
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-06-29 07:54:34 -0700

    defined classes for pickup scheduling and subscribe

--- laundry_app/models.py
+++ laundry_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class schedule(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.date} at {self.time}"
+    
+class Subscriber(models.Model):
+    email = models.EmailField()
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
