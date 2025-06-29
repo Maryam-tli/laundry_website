bf55202a1c22d8e5f1e00783bc88df6fbeb24de0
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-06-29 07:57:34 -0700

    Registered schedule and subscriber models in admin with custom display and search fields

--- laundry_app/admin.py
+++ laundry_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from laundry_app.models import *
 
 # Register your models here.
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('date', 'time', 'created_at', 'email', 'phone_number')
+    search_fields = ('date', 'time')
+    fields = ('date', 'time', 'email', 'phone_number')
+admin.site.register(schedule, ScheduleAdmin)
+
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscribed_at')
+    search_fields = ('email',)
+    fields = ('email',)
+admin.site.register(Subscriber, SubscriberAdmin)
