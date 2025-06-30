50fae25637476fc3bce47297bacf4e101a4056a7
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-06-30 08:39:39 -0700

    defined functions for listing posts and showing post detail

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog_app.models import *
 
 # Create your views here.
+def blog_view(request):
+    posts = Post.objects.filter(status=True).order_by('-published_date')
+    context = {'posts': posts,}
+    return render(request, 'blog.html', context)
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status=True)
+    context = {'post': post}
+    return render(request, 'blog-post.html', context)
