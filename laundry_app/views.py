75418295bd32f1befb03c526245e83e9457dece9
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-06-30 08:30:08 -0700

    fixed home, about, and services views with subscription and pickup forms

--- laundry_app/views.py
+++ laundry_app/views.py
@@ -4,13 +4,55 @@ from laundry_app.forms import *
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html')
+    if 'subscribe_submit' in request.POST:
+        form_2 = subscriberForm(request.POST)
+        if form_2.is_valid():
+            subscriber = form_2.save()
+            send_mail(
+                'Welcome to Our Newsletter – You’re Subscribed!',
+                f'Dear User,Thank you for subscribing to our newsletter! We will keep you updated with our latest news and offers.{subscriber.subscribed_at}',
+                'maryamtli@zohomail.com',
+                [subscriber.email],
+                fail_silently=False,
+            )
+            return redirect('home')
+    else:
+        form_2 = subscriberForm()
+    return render(request, 'home.html', {'form_2': form_2})
 
 def about_view(request):
-    return render(request, 'about.html')
+    if 'schedule_submit' in request.POST:
+        form = scheduleForm(request.POST)
+        if form.is_valid():
+            pickup = form.save()
+            send_mail(
+                'Pickup Scheduled',
+                f'Your pickup is scheduled on {pickup.date} at {pickup.time}.',
+                'maryamtli@zohomail.com',
+                [pickup.email],
+                fail_silently=False,
+            )
+            return redirect('about')
+
+    elif 'subscribe_submit' in request.POST:
+        form_2 = subscriberForm(request.POST)
+        if form_2.is_valid():
+            subscriber = form_2.save()
+            send_mail(
+                'Welcome to Our Newsletter – You’re Subscribed!',
+                f'Dear User,Thank you for subscribing to our newsletter! We will keep you updated with our latest news and offers.{subscriber.subscribed_at}',
+                'maryamtli@zohomail.com',
+                [subscriber.email],
+                fail_silently=False,
+            )
+            return redirect('about')
+    else:
+        form = scheduleForm()
+        form_2 = subscriberForm()
+    return render(request, 'about.html', {'form': form, 'form_2': form_2})
 
 def services_view(request):
-    if request.method == 'POST':
+    if 'schedule_submit' in request.POST:
         form = scheduleForm(request.POST)
         if form.is_valid():
             pickup = form.save()
@@ -22,7 +64,21 @@ def services_view(request):
                 fail_silently=False,
             )
             return redirect('services')
+
+    elif 'subscribe_submit' in request.POST:
+        form_2 = subscriberForm(request.POST)
+        if form_2.is_valid():
+            subscriber = form_2.save()
+            send_mail(
+                'Welcome to Our Newsletter – You’re Subscribed!',
+                f'Dear User,Thank you for subscribing to our newsletter! We will keep you updated with our latest news and offers.{subscriber.subscribed_at}',
+                'maryamtli@zohomail.com',
+                [subscriber.email],
+                fail_silently=False,
+            )
+            return redirect('services')
     else:
         form = scheduleForm()
+        form_2 = subscriberForm()
     
-    return render(request, 'services.html', {'form': form})
+    return render(request, 'services.html', {'form': form, 'form_2': form_2})
