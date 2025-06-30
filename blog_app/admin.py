48743b6a42c8092e230d45d69fcd438469f8e232
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-06-30 08:26:46 -0700

    registered Category, Author, and Post models

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from blog_app.models import *
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+admin.site.register(Category, CategoryAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bio')
+    search_fields = ('name',)
+    fields = ('name', 'bio', 'photo')
+admin.site.register(Author, AuthorAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author', 'created_date', 'published_date', 'status')
+    search_fields = ('title', 'content')
+    list_filter = ('category', 'author', 'status')
+    fields = ('title', 'content', 'slug','image', 'category', 'author', 'status', 'tags')
+admin.site.register(Post, PostAdmin)
