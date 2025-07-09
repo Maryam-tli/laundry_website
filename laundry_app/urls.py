ac0c5284febb81aeee2e111b1183479be0776c10
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-07-09 05:10:06 -0700

    defined a new path for contact

--- laundry_app/urls.py
+++ laundry_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('about/', about_view, name='about'),
     path('services/', services_view, name='services'),
+    path('contact/', contact_view, name='contact'),
 ]
