c668419a1b020ed9073be3c9cc8ef24b31d2e56e
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-07-09 01:37:22 -0700

    added some new paths

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,8 @@ from blog_app.views import *
 urlpatterns = [
     path('', blog_view, name='blog'),
     path('post/<slug:slug>/', post_detail, name='post_detail'),
+    path('search/', search_view, name='search'),
+    path('tag/<slug:slug>/', tagged_posts, name='tagged_posts'),
+    path('category/<slug:slug>/', blog_view, name='category'),
+    path('author/<slug:slug>/', author_view, name='author'),
 ]
