05c816009a8a3680a05b0ed8b16a797dbc853f9c
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-07-09 03:57:31 -0700

    defined the new path for summernote module

--- dj_folder/urls.py
+++ dj_folder/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('laundry_app.urls')),  # Include URLs from the laundry_app
     path('blog/', include('blog_app.urls')),  # Include URLs from the blog_app
+    path('summernote/', include('django_summernote.urls')),  # Include URLs for django_summernote
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
