c02ca787b18657dfcddd2aaa9d7420546d9b39bf
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-07-09 05:07:59 -0700

    added a form for contact table

--- laundry_app/forms.py
+++ laundry_app/forms.py
@@ -10,3 +10,32 @@ class subscriberForm(forms.ModelForm):
     class Meta:
         model = Subscriber
         fields = '__all__'
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = ['full_name', 'email', 'phone', 'subject', 'message', 'agree']
+        widgets = {
+            'message': forms.Textarea(attrs={
+                'rows': 4,
+                'cols': 50,
+                'placeholder': 'Write your message here...',
+                'class': 'form-control form-control-sm'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if not isinstance(field.widget, forms.CheckboxInput):
+                existing_classes = field.widget.attrs.get('class', '')
+                field.widget.attrs['class'] = f'{existing_classes} form-control form-control-sm'.strip()
+        self.fields['agree'].label = "I agree to the terms & conditions and privacy policy"
+
+
+    def clean_agree(self):
+        agree = self.cleaned_data.get('agree')
+        if not agree:
+            raise forms.ValidationError("You must agree to the terms and conditions.")
+        return agree
