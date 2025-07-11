d937fa53924ddf2dab089733f74ad2d83ad152ea
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-07-10 19:53:45 -0700

    trying to work with my database in Railway

--- dj_folder/settings.py
+++ dj_folder/settings.py
@@ -14,10 +14,6 @@ from pathlib import Path
 from decouple import config
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
-}
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -104,6 +100,24 @@ WSGI_APPLICATION = 'dj_folder.wsgi.application'
 #        'PORT': config('DB_PORT'),  # Database port
 #    }
 #}
+
+import os
+
+if config('DEBUG', default=False, cast=bool):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT', cast=int),
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
+    }
 
 
 # Password validation
