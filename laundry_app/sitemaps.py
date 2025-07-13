c3ee3c37c1c7401d0d7ee6f4fe9d12f4900d1e77
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-07-13 11:40:13 -0700

    defined sitemaps file for the html pages

---
+++ laundry_app/sitemaps.py
@@ -0,0 +1,11 @@
+from django.contrib import sitemaps
+from django.urls import reverse
+class StaticViewSitemap(sitemaps.Sitemap):
+    changefreq = "daily"
+    priority = 0.5
+
+    def items(self):
+        return ['home', 'about', 'services','contact']
+
+    def location(self, item):
+        return reverse(item)
