29136693dec481a26288acddbe13f178a07442cf
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-07-19 00:49:13 -0700

    changed STATIC_ROOT section

--- dj_folder/settings.py
+++ dj_folder/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'  # Directory for static files
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory for static files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'  # Directory for media files
 STATICFILES_DIRS = [
